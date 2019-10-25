89b4f348afc1b8a66d5cc41759877d743466363b
関口 陸 <sekiguchiriku@team-lab.com>
2019-10-25 11:45:39 +0900

    add channel

--- src/ex3/sync-package/channel/buffer/main.go
+++ src/ex3/sync-package/channel/buffer/main.go
@@ -1,1 +1,26 @@
-package buffer
+package mainl
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var stdoutBuf bytes.Buffer
+	defer stdoutBuf.WriteTo(os.Stdout)
+
+	intStream := make(chan int, 4)
+	go func() {
+		defer close(intStream)
+		defer fmt.Fprintln(&stdoutBuf, "Producer Done.")
+		for i := 0; i< 5; i++ {
+			fmt.Fprintf(&stdoutBuf, "Sending: %d\n", i)
+			intStream <- i
+		}
+	}()
+
+	for integer := range intStream {
+		fmt.Fprintf(&stdoutBuf, "Received %v.\n", integer)
+	}
+}
--- src/ex3/sync-package/channel/simple/main.go
+++ src/ex3/sync-package/channel/simple/main.go
@@ -1,1 +1,17 @@
-package simple
+package main
+
+import "fmt"
+
+func main() {
+	intStream := make(chan int)
+	go func() {
+		defer close(intStream)
+		for i := 1; i <= 5; i++ {
+			intStream <- i
+		}
+	}()
+
+	for integer := range intStream {
+		fmt.Printf("%v ", integer)
+	}
+}
